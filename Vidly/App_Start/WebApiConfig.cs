5ef9e2b32c15651204b6da50fc2c3f4e8b623fb5
Naveen Kengannar <naveen@cta.com>
2018-03-29 18:43:31 +0530

    Created Web Api

---
+++ Vidly/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using AutoMapper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Vidly
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver=new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
